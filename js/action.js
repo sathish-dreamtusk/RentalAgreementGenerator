83adcdd96d3cf3f584fae84ad04b480ff282432e
dhanasekaran.m <dhanasekaran.m@zohocorp.com>
2020-05-29 14:38:14 +0530

    action js Changes

--- js/action.js
+++ js/action.js
@@ -3,7 +3,7 @@ function loadFile(url,callback){
 }
 function generate() {
 
-    loadFile("https://docxtemplater.com/tag-example.docx",function(error,content){
+    loadFile("Resources/Rental_agreement.docx",function(error,content){
         if (error) { throw error };
 
         // The error object contains additional information when logged with JSON.stringify (it contains a properties object containing all suberrors).
@@ -40,11 +40,81 @@ function generate() {
             errorHandler(error);
         }
 
+        var location = document.getElementById("doc_location").value;
+        var documentedDay = document.getElementById("doc_day").value;
+
+        var enterdDocumentDate = formatDate(documentedDay)
+
+        var landlordDetail = document.getElementById("doc_landlord_detail").value;
+        var landlordAge = document.getElementById("doc_landlord_age").value;
+        var landlordResidingAt = document.getElementById("doc_landlord_residing_at").value; 
+
+        var tenantdDetail = document.getElementById("doc_tenant_detail").value;
+        var tenantAge = document.getElementById("doc_tenant_age").value;
+        var tenantResidingAt = document.getElementById("doc_tenant_residing_at").value;  
+
+        var addressOfRent = document.getElementById("doc_address_of_rented").value; 
+        var numberOfBedRooms = document.getElementById("doc_number_of_bed_room").value; 
+        var descriptionOfRentedPremise = document.getElementById("doc_description_of_rented_premise").value; 
+
+        var demisedCommenceFrom = document.getElementById("doc_demised_premise_commence_from").value;
+        var demisedValidTill = document.getElementById("doc_demised_premise_valid_till").value;
+        var demisedPeriodYears = document.getElementById("doc_demised_premise_period_years").value;
+
+        var enteredDemisedCommenceFrom = formatDate(demisedCommenceFrom);
+        var enteredDemisedValidTill = formatDate(demisedValidTill);
+
+        var monthlyRent = document.getElementById("doc_monthly_rent").value;
+        var rentLastDate = document.getElementById("doc_rent_last_date").value;
+        var maintanceCharge = document.getElementById("doc_maintainance_charge").value;
+        var detailsOfFurnishing = document.getElementById("doc_detail_of_furnishing").value;
+        var securityDeposit = document.getElementById("doc_security_deposit").value;
+
+        var rentalAgreementLockinPeriod = document.getElementById("doc_rental_agreement_lockin_period").value;
+        var rentalAgreementTerminatingPeriod = document.getElementById("doc_rental_agreement_terminate_period").value;
+        var rentalAgreementTerminatingNoticePeriod = document.getElementById("doc_rental_agreement_terminate_notice_period").value;
+        var arrearPaymentPeriod = document.getElementById("doc_arrear_payment_period").value;
+
+        var subjectOfJuri = document.getElementById("doc_subj_of_juri").value;
+        var executedDay = document.getElementById("doc_executed_day").value;
+
+        var enteredExecutedDate = formatDate(executedDay)
+
+        
         doc.setData({
-            doc_location: 'Mumbai',
-            doc_day: 'Monday',
-            doc_day_of: '21-06,1995',
-            doc_landlord_detail: 'Dhanasekaran'
+            doc_location: String(location),
+            doc_day: String(enterdDocumentDate),
+            
+            doc_landlord_detail: String(landlordDetail),
+            doc_landlord_age: String(landlordAge),
+            doc_landlord_residing_at: String(landlordResidingAt),
+
+            doc_tenant_detail: String(tenantdDetail),
+            doc_tenant_age: String(tenantAge),
+            doc_tenant_residing_at: String(tenantResidingAt),
+
+            doc_address_of_rented: String(addressOfRent),
+            doc_number_of_bed_room: String(numberOfBedRooms),
+            doc_description_of_rented_premise: String(descriptionOfRentedPremise),
+            doc_address_of_rented_premise: String(addressOfRent),
+
+            doc_demised_premise_commence_from: String(enteredDemisedCommenceFrom),
+            doc_demised_premise_valid_till: String(enteredDemisedValidTill),
+            doc_demised_premise_period_years: String(demisedPeriodYears),
+
+            doc_monthly_rent: String(monthlyRent),
+            doc_rent_last_date: String(rentLastDate),
+            doc_maintainance_charge: String(maintanceCharge),
+            doc_detail_of_furnishing: String(detailsOfFurnishing),
+            doc_security_deposit: String(securityDeposit),
+
+            doc_rental_agreement_lockin_period: String(rentalAgreementLockinPeriod),
+            doc_rental_agreement_terminate_period: String(rentalAgreementTerminatingPeriod),
+            doc_rental_agreement_terminate_notice_period: String(rentalAgreementTerminatingNoticePeriod),
+            doc_arrear_payment_period: String(arrearPaymentPeriod),
+
+            doc_subj_of_juri: String(subjectOfJuri),
+            doc_executed_day: String(enteredExecutedDate),
         });
         try {
             // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
@@ -58,8 +128,16 @@ function generate() {
         var out=doc.getZip().generate({
             type:"blob",
             mimeType: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-        }) //Output the document using Data-URI
-        saveAs(out,"output.docx")
+        }) 
+        var fileName = String(document.getElementById("exportingFilename").value);
+        if(fileName) {
+            fileName = fileName + ".docx"
+            saveAs(out, fileName)
+        } else {
+            saveAs(out, "Rental Agreement.docx")
+        }
+        
+        resetThisForm();
     })
 }
 
